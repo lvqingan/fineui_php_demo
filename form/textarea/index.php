51693b2d8e4ad3354d7e32a5ce7954ca4376fe72
lvqingan <lvqingan@gmail.com>
2017-09-02 16:49:14 +0800

    自动拓展高度

---
+++ form/textarea/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../../lib/FineUI/autoload.php';
+require_once '../../include/header.php';
+
+use \FineUI\F;
+
+?>
+
+<?php
+echo F::SimpleForm()
+    ->id('SimpleForm1')
+    ->bodyPadding(5)
+    ->enableCollapse(true)
+    ->title('简单表单')
+    ->labelAlign(\FineUI\Enums\LabelAlign::TOP)
+    ->width(550)
+    ->showHeader(true)
+    ->items(
+        F::TextArea()
+            ->id('TextArea1')
+            ->emptyText('文本框的高度会自动扩展')
+            ->label('自动扩展高度的文本输入框')
+            ->autoGrowHeight(true)
+            ->autoGrowHeightMin(100)
+            ->autoGrowHeightMax(200)
+    );
+?>
+<br/>
+<?php
+echo F::Label()->id('labResult')
+?>
+
+<?php \FineUI\PageContext::init() ?>
+
+<?php require_once '../../include/footer.php'; ?>
+
